4b2e39e05fa69f9363c1d5cef3f3361b03eb41dc
giuliazecchetin <giulia.zecchetin@gmail.com>
2026-03-12 14:52:13 +0100

    Initialized project.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Benvenuto su ForkIt 🍳</h1>
+      <p>Il mio ricettario sta prendendo forma!</p>
+    </div>
+  )
+}
+
+export default App
